88311ebbecfb3c4032413d5650ffecb9fa9a0a9f
GabrielChammas <gabriel.c@eurisko.net>
2021-12-23 21:18:34 +0200

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Gabriel Chammas on 12/23/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
