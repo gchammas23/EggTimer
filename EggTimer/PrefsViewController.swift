0654f956047fdec23641f2ffaaf2f0d1e30e4d80
GChammas23 <gabrielchammas23@gmail.com>
2021-12-25 01:21:44 +0200

    Add UI for preference page

---
+++ EggTimer/PrefsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by Gabriel Chammas on 12/25/21.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
