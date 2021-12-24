734c21d53ad5209400a60fa75f920da8191972e5
GChammas23 <gabrielchammas23@gmail.com>
2021-12-24 12:55:27 +0200

    Add outlets and actions for UI elements

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,7 +8,12 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var timeLeftField: NSTextField!
+    @IBOutlet weak var eggImageView: NSImageView!
+    @IBOutlet weak var startButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
+    @IBOutlet weak var resetButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +25,14 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    @IBAction func startButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonClicked(_ sender: Any) {
+    }
+    
 }
 
